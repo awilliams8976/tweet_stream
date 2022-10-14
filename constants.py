78223ed28b9c5c60f819aa5856c0bdd492afb96f
Adam Williams <adamwilliams8976@gmail.com>
2022-10-13 23:24:24 -0700

    Added in clean up to pfr qb data pull. Also, added in a method for loading to my MySQL instance.

--- constants.py
+++ constants.py
@@ -4,24 +4,9 @@ GITHUB_LINK = "https://github.com"
 COVID_COLUMNS = ["FIPS", "County", "Province", "Country", "Last_Update", "Latitude", "Longitude", "Confirmed", "Deaths", "Recovered", "Active", "Combined_Key", "Incident_Rate", "Case_Fatality_Ratio"]
 
 PFR_QB_MATCHUP_LINK = "https://www.pro-football-reference.com/fantasy/QB-fantasy-matchups.htm"
-PFR_QB_MATCHUP_HEADER = ["Player","Team","Status","Games","Games_Started","Snaps (%)","Pass_Completions","Pass_Attempts","Pass_Yards","Pass_Tds","Pass_Ints","Sacks","Rush_Attempts","Rush_Yards","Rush_Tds","Fantasy_Points","DraftKings_Points","FanDuel_Points","Home_Away","Opponent","Opponent_Rank","Fantasy_Points_Allowed","DraftKings_Points_Allowed","FanDuel_Points_Allowed","Fantasy_Points_Rank","DraftKings_Points_Rank","FanDuel_Points_Rank"]
-NFL_WEEKS = {
-    '2022-09-08':1,
-    '2022-09-15':2,
-    '2022-09-22':3,
-    '2022-09-29':4,
-    '2022-10-06':5,
-    '2022-10-13':6,
-    '2022-10-20':7,
-    '2022-10-27':8,
-    '2022-11-03':9,
-    '2022-11-10':10,
-    '2022-11-17':11,
-    '2022-11-24':12,
-    '2022-12-01':13,
-    '2022-12-08':14,
-    '2022-12-15':15,
-    '2022-12-22':16,
-    '2022-12-29':17,
-    '2022-01-05':18
+PFR_QB_MATCHUP_HEADER = ["Player","Team","Status","Games","Games_Started","Snaps","Pass_Completions","Pass_Attempts","Pass_Yards","Pass_Tds","Pass_Ints","Sacks","Rush_Attempts","Rush_Yards","Rush_Tds","Fantasy_Points","DraftKings_Points","FanDuel_Points","Home_Away","Opponent","Opponent_Rank","Fantasy_Points_Allowed","DraftKings_Points_Allowed","FanDuel_Points_Allowed","Fantasy_Points_Rank","DraftKings_Points_Rank","FanDuel_Points_Rank"]
+
+NFL_SCHEDULE = {
+    2022:{"start":"2022-09-08","end":"2023-01-05"},
+    2023:{"start":"2023-09-08","end":"2024-01-05"},
 }
--- pfr_scraper.py
+++ pfr_scraper.py
@@ -1,30 +1,96 @@
 import constants
+import datetime
+import math
+import numpy as np
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup, SoupStrainer
+from sqlalchemy import create_engine
 
-def get_pfr_qb_data(link):
-    data = requests.get(link).text
+class PfrData:
+    def __init__(self,link,header):
+        self.link = link
+        self.header = header
+        self.now = datetime.datetime.now()
 
-    soup = BeautifulSoup(data,"html.parser")
-    table = soup.find("table", class_="sortable stats_table")
+    def raw_pfr_qb_data(self):
+        data = requests.get(self.link).text
 
-    headers = constants.PFR_QB_MATCHUP_HEADER
-    rows = []
+        soup = BeautifulSoup(data,"html.parser")
+        table = soup.find("table", class_="sortable stats_table")
 
-    for row in table.tbody.find_all('tr'):
-        row_data = (row.find_all("th"))+(row.find_all("td"))
-        stats = []
+        headers = self.header
+        rows = []
 
-        for i in range(27):
-            stats.append(row_data[i].text)
+        for row in table.tbody.find_all('tr'):
+            row_data = (row.find_all("th"))+(row.find_all("td"))
+            stats = []
 
-        rows.append(stats)
+            for i in range(27):
+                stats.append(row_data[i].text)
 
-    df = pd.DataFrame(rows,columns=headers)
+            rows.append(stats)
+
+        df = pd.DataFrame(rows,columns=headers)
+
+        return df
+
+    def scrubbed_pfr_qb_data(self):
+        df = self.raw_pfr_qb_data()
+        df = df.replace(r'^\s*$', np.nan, regex=True)
+
+        convert_dict = {
+            "Games":int,
+            "Games_Started":int,
+            "Pass_Completions":float,
+            "Pass_Attempts":float,
+            "Pass_Yards":float,
+            "Pass_Tds":float,
+            "Pass_Ints":float,
+            "Sacks":float,
+            "Rush_Attempts":float,
+            "Rush_Yards":float,
+            "Rush_Tds":float,
+            "Fantasy_Points":float,
+            "DraftKings_Points":float,
+            "FanDuel_Points":float,
+            "Opponent_Rank":int,
+            "Fantasy_Points_Allowed":float,
+            "DraftKings_Points_Allowed":float,
+            "FanDuel_Points_Allowed":float,
+            "Fantasy_Points_Rank":int,
+            "DraftKings_Points_Rank":int,
+            "FanDuel_Points_Rank":int
+        }
+
+        df = df.astype(convert_dict)
+        df["Player"] = df["Player"].str.replace(r"[\.']*","",regex=True)
+        df["First_Name"] = df["Player"].str.extract(r"^(\w+)")
+        df["Last_Name"] = df["Player"].str.extract(r"[\s](\w+)")
+        df["Home_Away"] = df["Home_Away"].apply(lambda item: "away" if item=="@" else "home")
+        df["Snaps_%"] = df["Snaps"].str.extract(r"([0-9]*\.[0-9]*)").astype("float64")
+        df["Snaps"] = df["Snaps"].str.replace(r"(\([0-9]*.[0-9]*%\))","",regex=True).astype("int64")
+        df["Position"] = "qb"
+        df["Week_Number"] = self.calculate_nfl_week()
+        df = df.apply(lambda col: col.str.lower() if col.dtype=="object" else col)
+
+        return df
     
-    return df
+    def calculate_nfl_week(self):
+        year = self.now.year
+        schedule_start = datetime.datetime.strptime(constants.NFL_SCHEDULE[year]["start"],"%Y-%m-%d")
+        date_diff = abs(self.now-schedule_start)
+        days = date_diff.total_seconds()/86400
+
+        return math.ceil(days/7)
+
+    def pfr_data_to_mysql(self,user,password,host,port,database,table):
+        engine = create_engine(f"mysql://{user}:{password}@{host}:{port}/{database}")
+
+        df = self.scrubbed_pfr_qb_data()
+        return df.to_sql(table,con=engine,if_exists='append',index=False,method="multi")
 
 if __name__ == "__main__":
 
-    print(get_pfr_qb_data(constants.PFR_QB_MATCHUP_LINK))
+    qb_data = PfrData(constants.PFR_QB_MATCHUP_LINK,constants.PFR_QB_MATCHUP_HEADER)
+    qb_data.pfr_data_to_mysql(user="",password="",host="",port="",database="",table="")
